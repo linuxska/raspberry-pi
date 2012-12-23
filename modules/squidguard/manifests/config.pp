33104120789734bffc86c761d8ff8cbfda4bc20e
rchrd <richard.pot@me.com>
2012-12-23 12:03:53 +0100

    Added localnet definition to squid

--- modules/squidguard/manifests/config.pp
+++ modules/squidguard/manifests/config.pp
@@ -22,10 +22,21 @@ class squidguard::config inherits squidguard::params {
   }
 
   exec { 'link-squid-squidguard':
-    command   => "echo url_rewrite_program ${bin} -c ${confdir}/${conffile} >> ${squidconf}",
-    unless    => "grep ${conffile} ${squidconf}",
-    notify    => Class [ 'squidguard::service' ],
-    require   => File [ "${confdir}/${conffile}" ],
+    command => "echo url_rewrite_program ${bin} -c ${confdir}/${conffile} >> ${squidconf}",
+    unless  => "grep ${conffile} ${squidconf}",
+    notify  => Class [ 'squidguard::service' ],
+  }
+
+  exec { 'configure-squid-localnet':
+    command => "sed -i 's/#acl localnet src 192\.168\.0\.0\/16.*/acl localnet src 192\.168\.100\.0\/24/g' ${squidconf}",
+    onlyif  => "grep -F '#acl localnet src 192.168.0.0/16' ${squidconf}",
+    notify  => Class [ 'squidguard::service' ],
+  }
+
+  exec { 'allow-squid-localnet':
+    command => "sed -i 's/#http_access allow localnet/http_access allow localnet/g' ${squidconf}",
+    onlyif  => "grep -F '#http_access allow localnet' ${squidconf}",
+    notify  => Class [ 'squidguard::service' ],
   }
 
 }
