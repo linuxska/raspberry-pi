6397627ae2dd5065a5cca10a53ed78efb2b517a5
rchrd <richard.pot@me.com>
2012-12-26 09:05:08 +0100

    Removed unused packages

--- manifests/pi.pp
+++ manifests/pi.pp
@@ -3,4 +3,22 @@ node default {
   include bash
   include squidguard
 
-}
+  package { [
+    'lxappearance',
+    'lxde',
+    'lxde-common',
+    'lxde-core',
+    'lxde-icon-theme',
+    'lxmenu-data',
+    'lxinput',
+    'lxpanel',
+    'lxpolkit',
+    'lxrandr', 
+    'lxsession', 
+    'lxsession-edit', 
+    'lxshortcut', 
+    'lxtask', 
+    'lxterminal',
+    'wpasupplicant' ]:
+    ensure => absent,
+  }
